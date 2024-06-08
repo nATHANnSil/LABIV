feb58619f348a0bde31a7fbdbf25582e9ea0374d
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2024-06-07 21:50:32 -0300

    Add files via upload
    Faltante Passo 6 e adiante do roteiro 04 - aula 01

---
+++ roteiro04/FRONT_END/src/Index.jsx
@@ -0,0 +1,6 @@
+import React from "React";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.creatRoot(document.getElementById("root"));
+root.render(<App />);
