15de5c81cd6a23d05794d6acaf1c18c77a340d4c
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2024-06-14 21:19:02 -0300

    Delete roteiro04/roteiro03 directory

--- roteiro04/roteiro03/src/main/java/com/labdessoft/roteiro01/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.labdessoft.roteiro01.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.labdessoft.roteiro01.entity.Task;
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
-
-public class Repository {
-    
-}
