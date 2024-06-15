12bf21c28d701f55dcb5d922b0a2ff196b91a73e
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2024-06-14 21:20:24 -0300

    Update ToDoWrapper.jsx

--- roteiro04/FRONT_END/src/components/ToDoWrapper.jsx
+++ roteiro04/FRONT_END/src/components/ToDoWrapper.jsx
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
-import { TodoForm } from ' ./TodoForm';
-import { TodoList } from ' ./TodoList';
+import { TodoForm } from './TodoForm';
+import { TodoList } from './TodoList';
 import { v4 as uuidv4 } from "uuid";
 export const TodoWrapper = () => {
-    const [todos, setTodos] = useState([
-        { id: 1, description: 'Tarefa exemplo', completed: false }
-    ]);
+    const [todos, setTodos] = useState([]);
     const addTodo = (todo) => {
         setTodos([
             ...todos,
-            { id: uuidv4(), task: description, completed: false },
+            { id: uuidv4(), description: todo, completed: false },
         ]);
     }
+    const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
     return (
         <div className='TodoWrapper'>
             <h1>Lista de Tarefas</h1>
             <TodoForm addTodo={addTodo} />
-            {todos.map((item) =>
+            {todos.map((todo) =>
                 <TodoList
-                    key={item.id}
-                    task={item}
+                    key={todo.id}
+                    task={todo}
+                    deleteTodo={deleteTodo}
                 />
             )
             }
